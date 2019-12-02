c9b9eb6fa4beb4662e181a7d61f3e03a7d04f5e3
Mihael Safaric <mihael.safaric@gmail.com>
2019-12-02 02:04:14 +0100

    Cleanup

--- src/challenges/challenge-1/challenge-1.ts
+++ src/challenges/challenge-1/challenge-1.ts
@@ -1,13 +1,11 @@
-import { Observable, of, concat, empty } from 'rxjs';
-import { tap, map, mergeAll, flatMap, buffer, concatMap, exhaustMap, concatAll, scan, toArray, reduce, expand, take } from 'rxjs/operators';
+import { Observable, of, empty } from 'rxjs';
+import { map, reduce, expand } from 'rxjs/operators';
 
 export class SpacecraftModule {
   public runSequence(input$: Observable<any>): Observable<number> {
     return input$.pipe(
       map(this.getRequiredFuel),
-      reduce((acc, fuel) => {
-        return acc + fuel;
-      })
+      reduce(this.sumMasses)
     );
   }
 
@@ -18,13 +16,15 @@ export class SpacecraftModule {
         return fuel && fuel > 0 ? of(this.getRequiredFuel(fuel)) : empty();
       }),
       map((mass) => mass > 0 ? mass : 0),
-      reduce((acc, fuel) => {
-        return acc + fuel;
-      })
+      reduce(this.sumMasses)
     );
   }
 
   public getRequiredFuel(moduleMass: number): number {
     return Math.floor(moduleMass / 3) - 2;
+  }
+
+  private sumMasses(acc: number, fuel: number): number {
+    return acc + fuel;
   }
 }
