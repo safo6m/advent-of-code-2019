9d9002a61020bbf0b912d86cccb73555f08c234a
Mihael Safaric <mihael.safaric@gmail.com>
2019-12-03 01:11:35 +0100

    Solve challenge 2 part 2

--- src/challenges/challenge-2/challenge-2.spec.ts
+++ src/challenges/challenge-2/challenge-2.spec.ts
@@ -1,10 +1,9 @@
 import { of, from } from 'rxjs';
+import { map, concatAll } from 'rxjs/operators';
 import { readFileObservable } from '../../helpers/file-reader';
 import { ProgramAlarm } from './challenge-2';
-import { map, concatAll, tap } from 'rxjs/operators';
-import { parse } from 'path';
 
-fdescribe('Challenge 2 - 1202 Program Alarm', function() {
+describe('Challenge 2 - 1202 Program Alarm', function() {
   let instance: ProgramAlarm;
 
   beforeEach(() => {
@@ -49,6 +48,43 @@ fdescribe('Challenge 2 - 1202 Program Alarm', function() {
 
     instance.runSequence(sourceFile1, 12, 2).subscribe((result) => {
       expect(result).toEqual(6730673);
+      done();
+    });
+  });
+
+  it('should pass an input from challenge 2', function(done) {
+    const sourceFile1 = readFileObservable('./src/challenges/challenge-2/test-data/input-1.txt').pipe(
+      map((s) => s.split(',')),
+      map((s: Array<string>) => from(s)),
+      concatAll(),
+      map((s: string) => parseInt(s, 10))
+    );
+
+    instance.runSequence(sourceFile1, 37, 49).subscribe((result) => {
+      expect(result).toEqual(19690720);
+      done();
+    });
+  });
+});
+
+describe('Challenge 2 - part 2- 1202 Program Alarm', function() {
+  let instance: ProgramAlarm;
+
+  beforeEach(() => {
+    instance = new ProgramAlarm();
+  });
+
+  it('should find the correct noun and verb', function(done) {
+    const sourceFile1 = readFileObservable('./src/challenges/challenge-2/test-data/input-1.txt').pipe(
+      map((s) => s.split(',')),
+      map((s: Array<string>) => from(s)),
+      concatAll(),
+      map((s: string) => parseInt(s, 10))
+    );
+
+    instance.findVerbAndNoun(sourceFile1, 19690720).subscribe((result) => {
+      const calculatedResult: number = 100 * result.noun + result.verb;
+      expect(calculatedResult).toEqual(3749);
       done();
     });
   });
--- src/challenges/challenge-2/challenge-2.ts
+++ src/challenges/challenge-2/challenge-2.ts
@@ -1,13 +1,7 @@
-import { Observable, of, empty, merge, combineLatest, pipe, Subject, from } from 'rxjs';
-import { map, reduce, expand, flatMap, mergeAll, scan, buffer, bufferCount, toArray, concatAll, tap, take, takeLast, skip, skipLast, takeUntil, takeWhile } from 'rxjs/operators';
+import { Observable, of, empty, from } from 'rxjs';
+import { map, expand, flatMap, bufferCount, toArray, take, takeLast, skip, skipLast, takeWhile } from 'rxjs/operators';
+import { Operation } from './operations.enum';
 
-const operations = {
-  halt: 99,
-  addition: 1,
-  multiplication: 2
-}
-
-type Operation = number;
 type Position = number;
 type Opcode = [Operation, Position, Position, Position];
 
@@ -27,6 +21,34 @@ export class ProgramAlarm {
     );
   }
 
+  public findVerbAndNoun(input$: Observable<any>, finalResult: number): Observable<any> {
+    const pairs: Array<[number, number]> = [];
+
+    // TODO: find Rx way to do that
+    Array.from(Array(100).keys()).forEach((i: number) => {
+      Array.from(Array(100).keys()).forEach((j: number) => {
+        pairs.push([i, j]);
+      });
+    });
+
+    return from(pairs).pipe(
+      flatMap(([noun, verb]) => {
+        return of(true).pipe(
+          flatMap(this.runSequence.bind(this, input$, noun, verb)),
+          map((currentResult: number) => {
+            return {
+              noun,
+              verb,
+              finalResult: currentResult
+            };
+          })
+        );
+      }),
+      takeWhile((currentResult: any) => currentResult.finalResult !== finalResult, true),
+      takeLast(1)
+    )
+  }
+
   private runProgramIteration(wholeSequence: Array<number>, index: number): Observable<Array<number>> {
     return from(wholeSequence).pipe(
       bufferCount(4),
@@ -34,7 +56,7 @@ export class ProgramAlarm {
       take(1),
       takeWhile((opcode: Opcode) => {
         const operation: Operation = opcode[0];
-        return operation !== operations.halt;
+        return operation !== Operation.HALT;
       }),
       map(([operation, positionA, positionB, positionOutput]) => {
         const elementA: number = wholeSequence[positionA];
@@ -48,18 +70,26 @@ export class ProgramAlarm {
   }
 
   private calculate(elementA: number, elementB: number, operation: Operation): number {
-    if (operation === operations.addition) {
-      return elementA + elementB;
-    } else if (operation === operations.multiplication) {
-      return elementA * elementB;
+    let result: number;
+
+    switch(operation) {
+      case Operation.ADDITION:
+        result = elementA + elementB;
+        break;
+      case Operation.MULTIPLICATION:
+        result = elementA * elementB;
+        break;
+      default:
+        throw Error(`Unknown operation: ${operation}`);
     }
 
-    throw Error('Unknown operation');
+    return result;
   }
 
   private restoreProgram(wholeSequence: Array<number>, noun?: number, verb?: number): Array<number> {
-    wholeSequence[1] = noun || wholeSequence[1];
-    wholeSequence[2] = verb || wholeSequence[2];
-    return wholeSequence;
+    const sequence: Array<number> = [].concat(wholeSequence);
+    sequence[1] = noun || sequence[1];
+    sequence[2] = verb || sequence[2];
+    return sequence;
   }
 }
